35e36198d692b014814e95915f9ce80c0fe93351
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-25 15:43:51 +0100

    edited files

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,28 +5,28 @@ import EndingScreen from './EndingScreen';
 import Story from './Story';
 import Inventory from './Inventory';
 import CharacterStats from './CharacterStats';
-import Puzzle from './Puzzle';
-import Conversation from './Conversation';
-import SaveLoad from './SaveLoad';
-import Achievements from './Achievements';
-import Leaderboard from './Leaderboard';
-import Image from './Image';
 
 const GameContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 100vh;
 `;
 
 const ContentContainer = styled.div`
   text-align: center;
+  margin-bottom: 20px;
 `;
 
 const Game = ({ story }) => {
   const [currentStory, setCurrentStory] = useState(story[0]);
   const [isGameOver, setIsGameOver] = useState(false);
   const [inventory, setInventory] = useState([]);
+  const [characterStats, setCharacterStats] = useState({
+    health: 100,
+    energy: 100,
+  });
 
   const handleChoice = (choice) => {
     const newStory = story.find((s) => s.id === choice.nextStoryId);
@@ -41,6 +41,22 @@ const Game = ({ story }) => {
     const newItem = 'Puzzle Key';
     setInventory((prevInventory) => [...prevInventory, newItem]);
     console.log('Congratulations! You solved the puzzle and found a key.');
+  };
+
+  const handleFightDragon = () => {
+    // Decrease health and energy when fighting the dragon
+    const updatedHealth = characterStats.health - 50;
+    const updatedEnergy = characterStats.energy - 50;
+
+    // Check if the character has enough health and energy to continue
+    if (updatedHealth <= 0 || updatedEnergy <= 0) {
+      setIsGameOver(true); // Game over if health or energy is depleted
+    } else {
+      setCharacterStats({
+        health: updatedHealth,
+        energy: updatedEnergy,
+      });
+    }
   };
 
   return (
@@ -58,21 +74,21 @@ const Game = ({ story }) => {
             <ChoiceButton
               key={choice.id}
               text={choice.text}
-              onClick={() => handleChoice(choice)}
+              onClick={() => {
+                if (choice.id === 'fight-dragon') {
+                  handleFightDragon(); // Handle the fight choice
+                } else if (choice.id === 'puzzle-ending') {
+                  handlePuzzleSuccess(); // Handle the puzzle success
+                } else {
+                  handleChoice(choice); // Handle other choices
+                }
+              }}
             />
           ))}
 
           <Inventory items={inventory} />
-          <CharacterStats stats={{ health: 100, energy: 100 }} />
-          <Puzzle
-            puzzle={{ description: '', answer: '' }}
-            onSuccess={() => handlePuzzleSuccess()}
-          />
-          <Conversation dialogue={[]} choices={[]} />
-          <SaveLoad />
-          <Achievements achievements={[]} />
-          <Leaderboard />
-          <Image src="" alt="" />
+          <CharacterStats stats={characterStats} />
+          {/* Include other components as needed */}
         </>
       )}
     </GameContainer>
