39c4a821ba1bbab7e05db6ba2aeb7fc8d3ea69c9
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-25 09:57:21 +0100

    added files

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import ChoiceButton from './ChoiceButton';
 import EndingScreen from './EndingScreen';
-import Story from './Story';
+import Story from './Story'; // Import the Story component
 import Inventory from './Inventory';
 import CharacterStats from './CharacterStats';
 import Puzzle from './Puzzle';
@@ -20,11 +20,15 @@ const GameContainer = styled.div`
   height: 100vh;
 `;
 
+const ContentContainer = styled.div`
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
 const Game = ({ story }) => {
   const [currentStory, setCurrentStory] = useState(story[0]);
   const [isGameOver, setIsGameOver] = useState(false);
-  const [inventory, setInventory] = useState([]); // Define the inventory state and setter
-
+  const [inventory, setInventory] = useState([]);
 
   const handleChoice = (choice) => {
     const newStory = story.find((s) => s.id === choice.nextStoryId);
@@ -35,25 +39,23 @@ const Game = ({ story }) => {
     }
   };
 
-  // Define the handlePuzzleSuccess function
-const handlePuzzleSuccess = () => {
-  // You can perform actions when the player successfully solves a puzzle here
-  // For example, you can add an item to the player's inventory, update character stats, or advance the story.
-  // Here, we'll add an item to the inventory as an example.
-
-  // Create a new item to add to the inventory
-  const newItem = 'Puzzle Key';
-
-  // Update the inventory state to include the new item
-  setInventory((prevInventory) => [...prevInventory, newItem]);
-
-  // Optionally, update the story to provide feedback or continue the game
-  // You can set the next story segment here if needed.
-
-  // Display a success message to the player
-  console.log('Congratulations! You solved the puzzle and found a key.');
-};
-
+  const handlePuzzleSuccess = () => {
+    // You can perform actions when the player successfully solves a puzzle here
+    // For example, you can add an item to the player's inventory, update character stats, or advance the story.
+    // Here, we'll add an item to the inventory as an example.
+  
+    // Create a new item to add to the inventory
+    const newItem = 'Puzzle Key';
+  
+    // Update the inventory state to include the new item
+    setInventory((prevInventory) => [...prevInventory, newItem]);
+  
+    // Optionally, update the story to provide feedback or continue the game
+    // You can set the next story segment here if needed.
+  
+    // Display a success message to the player
+    console.log('Congratulations! You solved the puzzle and found a key.');
+  };
 
   return (
     <GameContainer>
@@ -61,8 +63,11 @@ const handlePuzzleSuccess = () => {
         <EndingScreen title={currentStory.title} text={currentStory.text} />
       ) : (
         <>
-          <h1>{currentStory.title}</h1>
-          <p>{currentStory.text}</p>
+          <ContentContainer>
+            <h1>{currentStory.title}</h1>
+            <p>{currentStory.text}</p>
+          </ContentContainer>
+
           {currentStory.choices.map((choice) => (
             <ChoiceButton
               key={choice.id}
@@ -70,16 +75,15 @@ const handlePuzzleSuccess = () => {
               onClick={() => handleChoice(choice)}
             />
           ))}
-          {/* Integrate the new components here */}
-          <Inventory items={[] /* Your inventory data */} />
-          <CharacterStats stats={{} /* Your character stats data */} />
-          <Puzzle puzzle={{ description: '', answer: '' } /* Your puzzle data */} onSuccess={() => handlePuzzleSuccess()} />
+
+          <Inventory items={inventory} />
+          <CharacterStats stats={{ health: 100, energy: 100 }} />
+          <Puzzle puzzle={{ description: '', answer: '' }} onSuccess={() => handlePuzzleSuccess()} />
           <Conversation dialogue={[]} choices={[]} />
           <SaveLoad />
-          <Achievements achievements={[] /* Your achievements data */} />
+          <Achievements achievements={[]} />
           <Leaderboard />
-          <Image src='' alt='' />
-          
+          <Image src="" alt="" />
         </>
       )}
     </GameContainer>
