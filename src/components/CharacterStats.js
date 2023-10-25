0f79a91934913141a31aa07205cc765dbd5c20fa
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-25 12:07:46 +0100

    edited files

--- src/components/CharacterStats.js
+++ src/components/CharacterStats.js
@@ -6,12 +6,21 @@ const StatsContainer = styled.div`
   padding: 10px;
   max-width: 300px;
   margin-top: 10px;
+  background-color: #f7f7f7;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const StatItem = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 5px;
+  font-weight: ${(props) => (props.isCritical ? 'bold' : 'normal')};
+  color: ${(props) => (props.isCritical ? 'red' : 'inherit')};
+`;
+
+const Icon = styled.span`
+  margin-right: 5px;
 `;
 
 const CharacterStats = ({ stats }) => {
@@ -19,13 +28,25 @@ const CharacterStats = ({ stats }) => {
     <StatsContainer>
       <h3>Character Stats</h3>
       {Object.entries(stats).map(([stat, value]) => (
-        <StatItem key={stat}>
+        <StatItem key={stat} isCritical={value < 10}>
+          <Icon>{getIconForStat(stat)}</Icon>
           <span>{stat}:</span>
           <span>{value}</span>
         </StatItem>
       ))}
     </StatsContainer>
   );
+};
+
+const getIconForStat = (stat) => {
+  // Define icons for different statistics
+  const icons = {
+    health: '❤️',
+    energy: '⚡',
+    // Add more icons for other statistics
+  };
+
+  return icons[stat] || 'ℹ️'; // Default icon for unknown stats
 };
 
 export default CharacterStats;
