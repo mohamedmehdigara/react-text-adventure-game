a1c84160032fb06e24d86aa8a37ddefe400d6e9f
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-26 11:13:19 +0100

    edited files

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,27 +1,17 @@
-// Define constants for story IDs
-// Define choice IDs as constants
-const Choices = {
-  GO_LEFT: 'go-left',
-  GO_RIGHT: 'go-right',
-  TAKE_TREASURE: 'take-treasure',
-  CONTINUE_EXPLORING: 'continue-exploring',
-  FIGHT_DRAGON: 'fight-dragon',
-  RUN_AWAY: 'run-away',
-};
-
 const story = [
   {
     id: 'start',
     title: 'Welcome to the Adventure!',
     text: 'You find yourself in a dark forest. What will you do?',
+    description: 'The moonlight barely filters through the dense canopy of trees. Eerie sounds echo in the distance.',
     choices: [
       {
-        id: Choices.GO_LEFT,
+        id: 'go-left',
         text: 'Go left',
         nextStoryId: 'left-path',
       },
       {
-        id: Choices.GO_RIGHT,
+        id: 'go-right',
         text: 'Go right',
         nextStoryId: 'right-path',
       },
@@ -31,14 +21,15 @@ const story = [
     id: 'left-path',
     title: 'The Left Path',
     text: 'You discover a hidden treasure! What will you do now?',
+    description: 'A glimmering chest adorned with intricate engravings emerges from the undergrowth.',
     choices: [
       {
-        id: Choices.TAKE_TREASURE,
+        id: 'take-treasure',
         text: 'Take the treasure',
         nextStoryId: 'treasure-ending',
       },
       {
-        id: Choices.CONTINUE_EXPLORING,
+        id: 'continue-exploring',
         text: 'Continue exploring',
         nextStoryId: 'start',
       },
@@ -48,39 +39,42 @@ const story = [
     id: 'right-path',
     title: 'The Right Path',
     text: 'You encounter a fierce dragon. What will you do?',
+    description: "The dragon's eyes burn with a fiery intensity, and its scales shimmer ominously.",
     choices: [
       {
-        id: Choices.FIGHT_DRAGON,
+        id: 'fight-dragon',
         text: 'Fight the dragon',
         nextStoryId: 'dragon-ending',
       },
       {
-        id: Choices.RUN_AWAY,
+        id: 'run-away',
         text: 'Run away',
         nextStoryId: 'start',
       },
-    
-    ]},
-  // Add more story segments as needed
-  // ...
+    ],
+  },
   {
     id: 'treasure-ending',
     title: 'Treasure Found!',
     text: 'Congratulations! You have found the hidden treasure and won the game!',
+    description: 'The chest reveals its secrets, and you bask in the glow of your newfound wealth.',
     choices: [],
   },
   {
     id: 'dragon-ending',
     title: 'Defeat',
     text: 'You bravely fought the dragon but sadly lost. Game over!',
+    description: "The dragon's fiery breath overwhelms you, and the forest falls silent in your defeat.",
     choices: [],
   },
   {
     id: 'no-more-choices-ending',
     title: 'Game Over',
     text: 'You have reached the end of your adventure. Thanks for playing!',
+    description: 'Your journey has come to an end, but many more stories await.',
     choices: [],
   },
+  // Add more story segments as needed with narrative attributes, images, and consequences
 ];
 
 export default story;
