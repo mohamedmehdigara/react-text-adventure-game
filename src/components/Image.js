9fadd6efc3fad64c05f5eb56e303ee7e15bd9dd4
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-27 10:32:42 +0100

    edited files

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Image = () => {
+const Image = ({ width = 200, height = 200, title = 'Image' }) => {
   return (
-    <svg width="200" height="200">
+    <svg
+      width={width}
+      height={height}
+      aria-label={title}
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 200 200"
+    >
       <circle cx="100" cy="100" r="80" fill="blue" />
-      <text x="40" y="120" fill="white">Image</text>
+      <text x="40" y="120" fill="white">
+        {title}
+      </text>
     </svg>
   );
 };
