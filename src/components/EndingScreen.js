00e368a98c4b962ee2995404ed204f073afd3790
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-27 09:30:51 +0100

    edited files

--- src/components/EndingScreen.js
+++ src/components/EndingScreen.js
@@ -7,12 +7,24 @@ const EndingContainer = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
+  background-color: #f7f7f7; /* Add a background color for improved readability */
+`;
+
+const Title = styled.h1`
+  font-size: 24px; /* Increase title font size for emphasis */
+  margin-bottom: 20px;
+  text-align: center; /* Center-align the title */
+`;
+
+const Text = styled.p`
+  font-size: 18px; /* Enhance text readability with a larger font size */
+  text-align: center; /* Center-align the text */
 `;
 
 const EndingScreen = ({ title, text }) => (
   <EndingContainer>
-    <h1>{title}</h1>
-    <p>{text}</p>
+    <Title>{title}</Title>
+    <Text>{text}</Text>
   </EndingContainer>
 );
 
