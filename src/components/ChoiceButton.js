21b3d0150418476cfbd9017d7c7e6e3ec7cb16b9
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-27 08:53:11 +0100

    edited files

--- src/components/ChoiceButton.js
+++ src/components/ChoiceButton.js
@@ -6,6 +6,22 @@ const Button = styled.button`
   padding: 10px 20px;
   font-size: 16px;
   cursor: pointer;
+  background-color: #4caf50; /* Add a background color */
+  color: white; /* Set text color to white */
+  border: none; /* Remove the default button border */
+  border-radius: 5px; /* Add rounded corners */
+  transition: background-color 0.3s, transform 0.1s; /* Add smooth transitions */
+
+  /* Hover effect */
+  &:hover {
+    background-color: #45a049; /* Darken the background color */
+    transform: scale(1.05); /* Slightly scale up on hover */
+  }
+
+  /* Active (clicked) effect */
+  &:active {
+    transform: scale(0.95); /* Slightly scale down when clicked */
+  }
 `;
 
 const ChoiceButton = ({ text, onClick }) => (
